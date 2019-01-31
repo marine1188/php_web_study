5f42cc5974d1184de57e1b7536bf2713d145be40
marine1188 <marine1188@yahoo.com>
2019-02-01 01:31:19 +0900

    아톰 연동

---
+++ su_yeon/test.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    http://dandy.godohosting.com/su_yeon/test.php
+    위에서 대로 주소창에 올리면 됨
+    /su_yeon/test.php 이거만 추가
+
+    <br>
+    이 폴더에서 html 이든 css이든 php 이던 하면 되고
+    <br>
+    디비 쓰고 싶으면 저저한테 말해주세요
+  </body>
+</html>
